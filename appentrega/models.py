359086f6991c4b8374a7de58f379071f55d12f57
Angelina-Bb <angelinabalatorre@gmail.com>
2022-11-20 19:36:24 -0300

    Creating models and BD

--- appentrega/models.py
+++ appentrega/models.py
@@ -3,14 +3,23 @@ from django.db import models
 # Create your models here.
 class Producto(models.Model):
     nombre = models.CharField(max_length=30)
+    droga = models.CharField(max_length=25)
     laboratorio = models.CharField(max_length=30)
     codigo = models.IntegerField()
     stock = models.IntegerField()
 
 class Carrito(models.Model):
     nombre = models.CharField(max_length=30)
+    codigo = models.IntegerField()
     cantidad = models.IntegerField()
 
 class Pedido(models.Model):
-    factura = models.IntegerField
-    cant_prod = models.IntegerField
+    factura = models.IntegerField()
+    cant_prod = models.IntegerField()
+
+class Contacto(models.Model):
+    nombre = models.CharField(max_length=25)
+    apellido = models.CharField(max_length=25)
+    telefono = models.IntegerField()
+    email = models.EmailField()
+    texto = models.CharField(max_length=500)
--- appentrega/views.py
+++ appentrega/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import Template, Context
+from django.template import loader
+import os
 # Create your views here.
 
 def inicio(request):
-    return HttpResponse('Bienvenido a la drogueria')
-    
+    ruta = os.path.abspath(os.path.curdir)
+    ruta_completa = ruta + r'\appentrega\templates\index.html'
+    archivo = open(ruta_completa)
+    plantilla = Template(archivo.read())
+    archivo.close()
+    contexto = Context()
+    documento = plantilla.render(contexto)
+    return HttpResponse(documento)
+
 def productos(request):
     return HttpResponse('Estas en productos.')
 
