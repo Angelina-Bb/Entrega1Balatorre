8938188747c15e946d9111547f14e20833e12dcb
Angelina-Bb <angelinabalatorre@gmail.com>
2022-11-22 18:41:57 -0300

    Updating models and BD

--- appentrega/models.py
+++ appentrega/models.py
@@ -7,19 +7,24 @@ class Producto(models.Model):
     laboratorio = models.CharField(max_length=30)
     codigo = models.IntegerField()
     stock = models.BooleanField()
+    precio = models.IntegerField()
+
 
 class Carrito(models.Model):
     nombre = models.CharField(max_length=30)
-    codigo = models.IntegerField()
+    precio = models.IntegerField()
     cantidad = models.IntegerField()
 
 class Pedido(models.Model):
     factura = models.IntegerField()
+    tipo = models.CharField(max_length=25)
     cant_prod = models.IntegerField()
+    total = models.IntegerField()
 
 class Contacto(models.Model):
     nombre = models.CharField(max_length=25)
     apellido = models.CharField(max_length=25)
     telefono = models.IntegerField()
     email = models.EmailField()
+    motivo = models.CharField(max_length=25)
     texto = models.CharField(max_length=500)
