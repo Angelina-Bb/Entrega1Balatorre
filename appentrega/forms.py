7e13ec7523c317a65b9b983a11fe74f97eb3b3fa
Angelina-Bb <angelinabalatorre@gmail.com>
2022-11-23 20:32:20 -0300

    Forms

--- appentrega/forms.py
+++ appentrega/forms.py
@@ -1,2 +1,11 @@
 from django import forms
 
+class ProductoFormulario(forms.Form):    
+    cliente = forms.CharField(max_length=25)
+    email = forms.EmailField()
+    nombre = forms.CharField(max_length=30)
+    droga = forms.CharField(max_length=25)
+    laboratorio = forms.CharField(max_length=30)
+    codigo = forms.IntegerField()
+    stock = forms.BooleanField()
+    precio = forms.IntegerField()
