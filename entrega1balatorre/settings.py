3e7e2cfd48393782e0a77da6fed6925bef462471
Angelina-Bb <angelinabalatorre@gmail.com>
2022-11-24 20:29:49 -0300

    protecting

--- entrega1balatorre/settings.py
+++ entrega1balatorre/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3=+je9-9i%7q5nr2+@+ee=lbvf6hu&u%qam8rnhrx)9_7u_ih!'
-
 # SECURITY WARNING: don't run with debug turned on in production!
+
+SECRET_KEY = config("SECRET_KEY")
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
